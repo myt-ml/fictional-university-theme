df6b1b0c604961ac82bd1acad2119810a9a3ea0f
myt-ml <maltahanml@gmail.com>
2024-10-19 19:57:00 +0300

    Wordpress development project

---
+++ single.php
@@ -0,0 +1,12 @@
+<?php  
+get_header();
+while(have_posts()){
+  the_post();?>
+  <h2><?php the_title(); ?></h2>
+  <p><?php the_content(); ?></p>
+  <p>Posted on: <?php the_date(); ?></p>
+  
+
+<?php }
+get_footer();
+?>
